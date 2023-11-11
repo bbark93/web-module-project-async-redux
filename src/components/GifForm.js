5cd1799e25cc0270d48d9f61cd052f690597905d
Brandon Barksdale <bbark93@gmail.com>
2023-11-10 20:23:36 -0500

    added search function

--- src/components/GifForm.js
+++ src/components/GifForm.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { connect } from "react-redux";
+import { useState } from "react";
 
-const GifForm = () => {
+import { getGifs } from "../actions";
+
+const GifForm = props => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.getGifs(searchTerm);
+  }
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  }
+
   return (
-    <form>
-      <input />
+    <form onSubmit={handleSubmit} >
+      <input onChange={handleChange} />
       <button>Search</button>
     </form>
   );
 };
 
-export default GifForm;
+export default connect(null, { getGifs })(GifForm);
